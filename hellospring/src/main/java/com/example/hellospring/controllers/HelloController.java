605357262f1c7416e43b3f8c53e51f4e14660e37
이유승 <yousi7@naver.com>
2021-04-24 23:19:02 +0900

    요청처리 연습

--- hellospring/src/main/java/com/example/hellospring/controllers/HelloController.java
+++ hellospring/src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -3,27 +3,39 @@ package com.example.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+//메서드 단독 매핑: 메서드의 앞에 RequestMapping을 부여
 @Controller
 public class HelloController {
-
-	// 요청을 받아야 한다
-	// url 패턴  /hello 에 반응하고
-	// 파라미터로 name 를 받는다
-	// 예 : /hello?name=홍길동
+//	요청을 받아야 한다
+//	url 패턴 /hello에 반응하고
+//	파라미터로 name을 받는다
+//	예: /hello?name=홍길동
+@RequestMapping("/hello")
+public ModelAndView hello(@RequestParam String name) {
+	//	Spring의 기본 응답 객체는 ModelAndView
+	//	데이터(Model)와 뷰 객체의 정보를 함께 가지고 있다.
+	ModelAndView mav = new ModelAndView();
 	
-	@RequestMapping("/hello")
-	public ModelAndView hello(@RequestParam String name) {
-		// Spring 기본 응답 객체는  ModelAndView
-		// 데이터(Model) 의 뷰 객체의 정보를 함께 가지고 있다.
-		ModelAndView mav = new ModelAndView();
-		
-		// 데이터를 설정
-		mav.addObject("message", "hello, " + name);			//  servlet 에서 attribute 추가와 비슷하다고 생각하면 됨
-		mav.setViewName("WEB-INF/views/hello.jsp");
-		
-		return mav;
-		
-	}
+	//	데이터를 설정
+	mav.addObject("message", "Hello, " + name);	//	Servlet에서 attribute 추가와 비슷
+	mav.setViewName("/WEB-INF/views/hello.jsp");
+	
+	return mav;
+}
+
+//	String을 반환하면 -> 기본적으로는 뷰 이름을 의미
+@RequestMapping("/hello2")
+public String hello2() {
+	return "/WEB-INF/views/hello.jsp";
+}
+
+//	ResponseBody -> 리턴된 객체를 바로 응답으로 출력
+@ResponseBody
+@RequestMapping("/hello3")
+public String hello3() {
+	return "<h3>Direct Response</h3>";
+}
 }
